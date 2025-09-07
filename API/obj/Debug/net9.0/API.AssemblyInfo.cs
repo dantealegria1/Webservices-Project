00c0bb9425c329faa674c0fce37944d0cde88d93
Dante Alegria <dalegria@data-keeper.com>
2025-09-07 13:11:44 -0600

    2025-09-07

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7006d033c0ca744b17d19f5d1d9927a697d0acbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6388e71c8e82e65396d1220b958b60cf446531a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
