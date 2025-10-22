5bf4f6378b49faab6039f5515c61bcae41615d37
Dante Alegria <dalegria@data-keeper.com>
2025-10-21 19:30:27 -0600

    Fixing Angular version issues

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,33 +1,35 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection, PLATFORM_ID } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { InitService } from '../core/services/init-service';
 import { lastValueFrom } from 'rxjs';
 import { errorInterceptor } from '../core/interceptors/error-interceptor';
+import { isPlatformBrowser } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection(),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideHttpClient(withInterceptors([errorInterceptor]), withFetch()),
+
     provideAppInitializer(async () => {
       const initService = inject(InitService);
+      const platformId = inject(PLATFORM_ID);
 
       return new Promise<void>((resolve) => {
         setTimeout(async () => {
           try {
-            return lastValueFrom(initService.init())
+            await lastValueFrom(initService.init());
           } finally {
-            const splash = document.getElementById("initial-splash");
-
-            if (splash) {
-              splash.remove();
+            if (isPlatformBrowser(platformId)) {
+              const splash = document.getElementById("initial-splash");
+              splash?.remove();
             }
-
             resolve();
           }
         }, 500);
       });
-    })  ]
+    })
+  ]
 };
