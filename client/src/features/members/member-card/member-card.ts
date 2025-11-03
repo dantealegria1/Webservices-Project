c9d78f7386fea49037ca15ffdcbb0d036e45de14
Arturo Barajas <basaar.24@gmail.com>
2025-11-03 07:41:30 -0600

    2025-11-27

--- client/src/features/members/member-card/member-card.ts
+++ client/src/features/members/member-card/member-card.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Member } from '../../../types/member';
 import { RouterLink } from "@angular/router";
+import { AgePipe } from '../../../core/pipes/age-pipe';
 
 @Component({
-selector: 'app-member-card',
-  imports: [RouterLink],
-templateUrl: './member-card.html',
-styleUrl: './member-card.css'
+  selector: 'app-member-card',
+  imports: [RouterLink, AgePipe],
+  templateUrl: './member-card.html',
+  styleUrl: './member-card.css'
 })
 export class MemberCard {
 member = input.required<Member>();
