1a089a9df53504349d8068de2098bfa0030ac87b
Dante Alegria <dalegria@datakeeper.com>
2025-11-27 22:17:46 -0600

    test

---
+++ tests/API.UnitTests/GlobalTestSetup.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API.UnitTests;
+
+[SetUpFixture]
+public class GlobalTestSetup
+{
+    public static AppDbContext AppDbContext { get; private set; }
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        DbContextOptions<AppDbContext> options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite("Data source=dating.db")
+            .Options;
+
+        AppDbContext = new AppDbContext(options);
+        await AppDbContext.Database.MigrateAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDown()
+    {
+        await AppDbContext.DisposeAsync();
+    }
+}
