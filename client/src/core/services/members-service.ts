1b5c9045fd38de6e625fa62f3ee286ed321f8b93
Dante Alegria <dalegria@data-keeper.com>
2025-11-27 20:29:51 -0600

    2025-11-27

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -1,9 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member } from '../../types/member';
+import { Member, Photo } from '../../types/member';
 import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +11,15 @@ export class MembersService {
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
 
-
   getMember(id: string): Observable<Member> {
     return this.http.get<Member>(this.baseUrl + "members/" + id);
   }
 
   getMembers(): Observable<Member[]> {
     return this.http.get<Member[]>(this.baseUrl + "members");
+  }
+
+  getPhotos(id: string) {
+    return this.http.get<Photo[]>(`${this.baseUrl}members/${id}/photos`);
   }
 }
